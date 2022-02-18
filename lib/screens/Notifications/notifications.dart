dec8e74fb70595c019bc1a8b63ff091f4d95dc79
triipaathii <myself10121990@gmail.com>
2022-02-19 01:55:54 +0530

    notification page added

---
+++ lib/screens/Notifications/notifications.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Notifications extends StatefulWidget {
+  const Notifications({Key? key}) : super(key: key);
+
+  @override
+  _NotificationsState createState() => _NotificationsState();
+}
+
+class _NotificationsState extends State<Notifications> {
+  @override
+  Widget build(BuildContext context) {
+
+    SystemChrome.setSystemUIOverlayStyle(
+        const SystemUiOverlayStyle(
+          statusBarColor: Colors.black,
+          //color set to transperent or set your own color
+          statusBarIconBrightness: Brightness.dark,
+          //set brightness for icons, like dark background light icons
+        )
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Notifications",
+          style: TextStyle(
+            fontFamily: "Poppins",
+          ),
+        ),
+        backgroundColor: Colors.black,
+      ),
+      // body: SafeArea(),
+    );
+  }
+}
